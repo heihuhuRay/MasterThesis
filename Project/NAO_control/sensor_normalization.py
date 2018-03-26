5ff094e597784da21e97326115c719216ef98a19
heihuhuRay <178200901@qq.com>
2018-03-26 21:57:11 +0200

    update

--- Project/NAO_control/sensor_normalization.py
+++ Project/NAO_control/sensor_normalization.py
@@ -40,32 +40,32 @@ range_R = [275.0, 219.0, 170.0, 188.0,  86.0, 104.0, 132.0]
 range_L = [237.0, 254.0, 300.0, 448.0, 435.0, 296.0, 381.0]
 
 ReadlistData = [
-              ## Head Touch
-              "Device/SubDeviceList/Head/Touch/Front/Sensor/Value",  # 0
-              "Device/SubDeviceList/Head/Touch/Rear/Sensor/Value",   # 1
-              "Device/SubDeviceList/Head/Touch/Middle/Sensor/Value", #2
-              ## LeftHandTouch
-              "Device/SubDeviceList/LHand/Touch/Back/Sensor/Value", #3 
-              "Device/SubDeviceList/LHand/Touch/Left/Sensor/Value", #4 
-              "Device/SubDeviceList/LHand/Touch/Right/Sensor/Value", #5
-              ## RightHandTouch 
-              "Device/SubDeviceList/RHand/Touch/Back/Sensor/Value", #6
-              "Device/SubDeviceList/RHand/Touch/Left/Sensor/Value", #7 
-              "Device/SubDeviceList/RHand/Touch/Right/Sensor/Value" #8 
-              ## LeftFSR 
-              "Device/SubDeviceList/LFoot/FSR/FrontLeft/Sensor/Value", #9
-              "Device/SubDeviceList/LFoot/FSR/FrontRight/Sensor/Value", #10 
-              "Device/SubDeviceList/LFoot/FSR/RearLeft/Sensor/Value",  #11 
-              "Device/SubDeviceList/LFoot/FSR/RearRight/Sensor/Value",  #12
-              ## RightFSR 
-              "Device/SubDeviceList/RFoot/FSR/FrontLeft/Sensor/Value", #13 
-              "Device/SubDeviceList/RFoot/FSR/FrontRight/Sensor/Value", #14
-              "Device/SubDeviceList/RFoot/FSR/RearLeft/Sensor/Value",  #15
-              "Device/SubDeviceList/RFoot/FSR/RearRight/Sensor/Value", #16 
-              ## Body Angle
-              "Device/SubDeviceList/InertialSensor/AngleX/Sensor/Value", #17 
-              "Device/SubDeviceList/InertialSensor/AngleY/Sensor/Value"  #18 
-           ]
+          ## Head Touch
+          "Device/SubDeviceList/Head/Touch/Front/Sensor/Value",  # 0
+          "Device/SubDeviceList/Head/Touch/Rear/Sensor/Value",   # 1
+          "Device/SubDeviceList/Head/Touch/Middle/Sensor/Value", #2
+          ## LeftHandTouch
+          "Device/SubDeviceList/LHand/Touch/Back/Sensor/Value", #3 
+          "Device/SubDeviceList/LHand/Touch/Left/Sensor/Value", #4 
+          "Device/SubDeviceList/LHand/Touch/Right/Sensor/Value", #5
+          ## RightHandTouch 
+          "Device/SubDeviceList/RHand/Touch/Back/Sensor/Value", #6
+          "Device/SubDeviceList/RHand/Touch/Left/Sensor/Value", #7 
+          "Device/SubDeviceList/RHand/Touch/Right/Sensor/Value" #8 
+          ## LeftFSR 
+          "Device/SubDeviceList/LFoot/FSR/FrontLeft/Sensor/Value", #9
+          "Device/SubDeviceList/LFoot/FSR/FrontRight/Sensor/Value", #10 
+          "Device/SubDeviceList/LFoot/FSR/RearLeft/Sensor/Value",  #11 
+          "Device/SubDeviceList/LFoot/FSR/RearRight/Sensor/Value",  #12
+          ## RightFSR 
+          "Device/SubDeviceList/RFoot/FSR/FrontLeft/Sensor/Value", #13 
+          "Device/SubDeviceList/RFoot/FSR/FrontRight/Sensor/Value", #14
+          "Device/SubDeviceList/RFoot/FSR/RearLeft/Sensor/Value",  #15
+          "Device/SubDeviceList/RFoot/FSR/RearRight/Sensor/Value", #16 
+          ## Body Angle
+          "Device/SubDeviceList/InertialSensor/AngleX/Sensor/Value", #17 
+          "Device/SubDeviceList/InertialSensor/AngleY/Sensor/Value"  #18 
+       ]
 ######################################
 # Connect to the module ALMemoryProxy
 memProxy = ALProxy("ALMemory", NAOIP, PORT)
@@ -77,33 +77,33 @@ time.sleep(2)
 while True:
     # 摸摸头
     if (listValRead[1]):
-        print('Testing has been stoped by toutching the rear tactile head sensor.\n')
-        break
+    print('Testing has been stoped by toutching the rear tactile head sensor.\n')
+    break
 
     FrontTactileON   = memProxy.getData('Device/SubDeviceList/Head/Touch/Front/Sensor/Value')
     if FrontTactileON == 1:
-        break 
+    break 
 
     data = memProxy.getData("WristForceSensor")
 
     # normalization
     R_nor_data = [  (data[0][0] - min_R[0])/range_R[0],
-                    (data[0][1] - min_R[1])/range_R[1],
-                    (data[0][2] - min_R[2])/range_R[2],
-                    (data[0][3] - min_R[3])/range_R[3],
-                    (data[0][4] - min_R[4])/range_R[4],
-                    (data[0][5] - min_R[5])/range_R[5],
-                    (data[0][6] - min_R[6])/range_R[6],
-                ]
+            (data[0][1] - min_R[1])/range_R[1],
+            (data[0][2] - min_R[2])/range_R[2],
+            (data[0][3] - min_R[3])/range_R[3],
+            (data[0][4] - min_R[4])/range_R[4],
+            (data[0][5] - min_R[5])/range_R[5],
+            (data[0][6] - min_R[6])/range_R[6],
+        ]
 
     L_nor_data = [  (data[1][0] - min_L[0])/range_L[0],
-                    (data[1][1] - min_L[1])/range_L[1],
-                    (data[1][2] - min_L[2])/range_L[2],
-                    (data[1][3] - min_L[3])/range_L[3],
-                    (data[1][4] - min_L[4])/range_L[4],
-                    (data[1][5] - min_L[5])/range_L[5],
-                    (data[1][6] - min_L[6])/range_L[6],
-                ]
+            (data[1][1] - min_L[1])/range_L[1],
+            (data[1][2] - min_L[2])/range_L[2],
+            (data[1][3] - min_L[3])/range_L[3],
+            (data[1][4] - min_L[4])/range_L[4],
+            (data[1][5] - min_L[5])/range_L[5],
+            (data[1][6] - min_L[6])/range_L[6],
+        ]
 
     print("[R_1, R_2, R_3, R_4, R_5, R_6, R_7]")
     print(R_nor_data)
--- Project/SensorData_Processing/pi_read_sensor.py
+++ Project/SensorData_Processing/pi_read_sensor.py
@@ -25,42 +25,42 @@ left_sensor = [0]*num_sensor_right
 
 
 while True:
-        while True:
-                button_pressed = GPIO.input(21)
-                if button_pressed == 1:
-                        print("Bottom pressed for starting..")
-                        time.sleep(2)
-                        break
-        sock1 = socket.socket(socket.AF_INET, # Internet
-                              socket.SOCK_DGRAM) # UDP
-        count = 0
-        start_time = time.time()
-        total_time = 0
+    while True:
+        button_pressed = GPIO.input(21)
+        if button_pressed == 1:
+            print("Bottom pressed for starting..")
+            time.sleep(2)
+            break
+    sock1 = socket.socket(socket.AF_INET, # Internet
+                  socket.SOCK_DGRAM) # UDP
+    count = 0
+    start_time = time.time()
+    total_time = 0
 
 
-        while True:
-                # read the analog pin
-                for i in range(0,num_sensor_left):
-                        right_sensor[i]  = readadcA(i)
-                        print('right sensor')
-                        print(right_sensor)
-                for i in range(0,num_sensor_right):
-                        left_sensor[i] = readadcB(i)
+    while True:
+        # read the analog pin
+        for i in range(0,num_sensor_left):
+            right_sensor[i]  = readadcA(i)
+            print('right sensor')
+            print(right_sensor)
+        for i in range(0,num_sensor_right):
+            left_sensor[i] = readadcB(i)
 
-                sock1.sendto(str([right_sensor,left_sensor]), (UDP_IP, UDP_PORT))
+        sock1.sendto(str([right_sensor,left_sensor]), (UDP_IP, UDP_PORT))
 
-                count = count +1
-                time.sleep(0.01)
-                total_time = time.time()-start_time
+        count = count +1
+        time.sleep(0.01)
+        total_time = time.time()-start_time
 
-                button_pressed = GPIO.input(21)
-                if button_pressed == 0:
-                        print("Bottom pressed for starting..")
-                        time.sleep(2)
-                        break
-        break
+        button_pressed = GPIO.input(21)
+        if button_pressed == 0:
+            print("Bottom pressed for starting..")
+            time.sleep(2)
+            break
+    break
 
-        print(" nbr. of reading per second:", count/total_time) 
+    print(" nbr. of reading per second:", count/total_time) 
 
 
 
